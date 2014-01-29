5bdfa38e1b5e0ff9dd587322958b67f28e79e84a
Zhiyuan Wang <zywang@u.northwestern.edu>
2014-01-29 01:00:53 -0600

    final

--- c4_5.py
+++ c4_5.py
@@ -1,4 +1,5 @@
 #from scipy import mode 
+# coding=utf-8
 import csv
 import math
 import operator
@@ -7,24 +8,41 @@ import copy
 from collections import Counter
 
 class node(object):
+	"""A class for decision tree structure
+
+	Attributes:
+		val: value of the node, may be list to store condition
+		child: list of subtree
+	"""
 	def __init__(self, val, child):
 		self.val = val
 		self.child = child
 	def __str__(self):
 		return 'val:'+str(self.val) +'\n'+'child:'+ str(self.child)
 
-class C4_5:
+class C4_5(object):
+	"""A class for all the implementation of c4.5 dtree algorithm
+
+	Attributes:
+		tree: store the decision tree as tree structure
+		format: the form of attributes, i.e. ['numeric', 'nominal', 'nominal']
+		attributes: a list of attributes of the input data
+		train_set: the input training data
+		data_len: the length of train_set
+		rule: the generated rule
+	"""
 
 	def __init__(self, train_set, format, rule):
+		"""Initialize C4_5 with training data, format, rule(can be empty)"""
 		self.tree = node(None, [])
 		"""store the headerline in attributes
 	    store the data in train_set 
 	    """
 		train_set = list(train_set)
+		self.format = format
 		self.attributes = train_set[0][:-1]
 		self.train_set = train_set[1:]
 		self.data_len = len(self.train_set)
-		self.format = format
 		self.rule = rule
 		"""it's a recursive method,
 	    when first call it, the input tree should be self.tree
@@ -35,15 +53,15 @@ class C4_5:
 	    """
 	def train(self, examples, tree, attributes, default):
 		# when no examples return default
-		if not examples:
+		if len(examples) == 0:
 			#print 'A'
 			return default
 		# if all examples have the same classification return the classification
-		elif len(unique([item[-1] for item in examples])) == 1:
+		elif allequal([item[-1] for item in examples]):
 			#print 'B'
 			return examples[0][-1]
 		# when attributes is empty then return mode(examples)
-		elif not attributes:
+		elif len(attributes) == 0 or len(examples) < 100:
 			#print 'C'
 			return mode([item[-1] for item in examples])[0][0]
 		else:
@@ -62,10 +80,10 @@ class C4_5:
 					branch = node([best[0], '==', v], [subtree])
 					tree.child.append(branch)
 			else:
-				print 'A'
-				print 'examples: '+ str(len(examples))
+				#print 'A'
+				#print 'examples: '+ str(len(examples))
 				exp1 = [item for item in examples if float(item[idx]) > best[2]]
-				print 'exp1: '+str(len(exp1))
+				#print 'exp1: '+str(len(exp1))
 				default = mode([item[-1] for item in examples])[0][0]
 				if len(exp1) == len(examples):
 					return default
@@ -81,54 +99,61 @@ class C4_5:
           
    	def choose_attr(self, attributes, examples):
    		max_gain = -10
-   		#min_rem = 100
-   		data_len = len(examples)
+   		data_len = float(len(examples))
    		group = [item[-1] for item in examples]
-   		infor = entropy([float(group.count('1'))/data_len, float(group.count('0'))/data_len])
+   		infor = entropy([group.count('1')/data_len, group.count('0')/data_len])
    		for attr in attributes:
-   			print '.'
-			idx = self.attributes.index(attr)
-			#mode_1 = mode([item[idx] for item in examples if item[-1] == '1'])[0][0]
-			#mode_0 = mode([item[idx] for item in examples if item[-1] == '0'])[0][0]
-			mode_exp = mode([item[idx] for item in examples])[0][0]
-			for item in examples:
-				if item[idx] == '?':
-					if item[-1] == '1':
-						item[idx] = mode_exp
-					else:
-						item[idx] = mode_exp
-			#rem = 0
-			gain = infor
-			if self.format[idx] == 'nominal':
-				for i in unique([item[idx] for item in examples]):
-					exp = [item[-1] for item in examples if item[idx] == str(i)]
-					split_len = len(exp)
-					gain -= float(split_len)/data_len * entropy([ float(exp.count('1'))/split_len, float(exp.count('0'))/split_len ])
-				#print rem
-				if gain > max_gain:
-					max_gain = gain
-					best = [attr, 'nom']
-			else:
-				exps = list(examples)
-				for item in exps:
-					item[idx] = float(item[idx])
-				exps.sort(key=operator.itemgetter(idx))
-				split_candidate = self.split(exps, idx)
-				for thresh in split_candidate:
-					#rem = 0
-					exp1 = [item[-1] for item in exps if float(item[idx]) > thresh]
-					exp2 = [item[-1] for item in exps if float(item[idx]) <= thresh]
-					len1 = len(exp1)
-					len2 = len(exp2)
-					gain -= float(len1)/data_len * entropy([ float(exp1.count('1'))/len1, float(exp1.count('0'))/len1 ])
-					gain -= float(len2)/data_len * entropy([ float(exp2.count('1'))/len2, float(exp2.count('0'))/len2 ])
-					#print rem
-					if gain > max_gain:
-						max_gain = gain
-						best = [attr, 'num', thresh]
-		print max_gain
-		if max_gain <= 0:
-			return 0
+   			idx = self.attributes.index(attr)
+   			gain = infor
+   			if self.format[idx] == 'nominal':
+   				mode_exp = mode([item[idx] for item in examples])[0][0]
+   				#mode_0 = mode([item[idx] for item in examples if item[-1]=='0'])[0][0]
+   				for item in examples:
+   					if item[idx] == '?':
+   						item[idx] = mode_exp
+   				for i in unique([item[idx] for item in examples]):
+   					exp = [item[-1] for item in examples if item[idx] == i]
+   					split_len = float(len(exp))
+   					gain -= split_len/data_len * entropy([ exp.count('1')/split_len, exp.count('0')/split_len ])
+   				if gain > max_gain:
+   					max_gain = gain
+   					best = [attr, 'nom']
+   			else:
+   				num_lst = [float(item[idx]) for item in examples if item[idx] != '?']
+   				#num_lst_0 = [float(item[idx]) for item in examples if item[idx] != '?' and item[-1]=='0']
+   				mean = sum(num_lst) / len(num_lst)
+   				#mean_0 = sum(num_lst_0) / len(num_lst_0)
+   				#mode_exp = float(mode([item[idx] for item in examples])[0][0])
+   				exps = list(examples)
+   				for item in exps:
+   					if item[idx] == '?':
+   						item[idx] = mean
+   					else:
+   						item[idx] = float(item[idx])
+   				#exps = list(examples)
+   				#for item in exps:
+   				#	item[idx] = float(item[idx])
+   				exps.sort(key=operator.itemgetter(idx))
+   				split_candidate = self.split(exps, idx)
+   				for thresh in split_candidate:
+   					#print '*************************************'
+   					#print thresh
+   					gain = infor
+   					exp1 = [item[-1] for item in exps if float(item[idx]) > thresh]
+   					exp2 = [item[-1] for item in exps if float(item[idx]) <= thresh]
+   					len1 = float(len(exp1))
+   					len2 = float(len(exp2))
+   					if len1==0 or len2==0:
+   						gain = 0
+   					else:
+   						gain -= len1/data_len * entropy([ exp1.count('1')/len1, exp1.count('0')/len1 ])
+   						gain -= len2/data_len * entropy([ exp2.count('1')/len2, exp2.count('0')/len2 ])
+   					if gain > max_gain:
+   						max_gain = gain
+   						best = [attr, 'num', thresh]
+   		print max_gain
+   		if max_gain <= 0:
+   			return 0
    		return best
 
    	
@@ -136,39 +161,52 @@ class C4_5:
    		exp = copy.deepcopy(examples)
    		for attr in self.attributes:
    			idx = self.attributes.index(attr)
-   			mode_1 = mode([item[idx] for item in examples if item[-1] == '1'])[0][0]
-   			mode_0 = mode([item[idx] for item in examples if item[-1] == '0'])[0][0]
-   			for item in exp:
-   				if item[idx] == '?':
-   					if item[-1] == '1':
-   						item[idx] = mode_1
-   					else:
-   						item[idx] = mode_0
-   		return exp	
+   			if self.format[idx] == 'nominal':
+   				mode_exp = mode([item[idx] for item in exp])[0][0]
+   				#mode_0 = mode([item[idx] for item in exp if item[-1]=='0'])[0][0]
+   				for item in exp:
+   					if item[idx] == '?':
+   						item[idx] = mode_exp
+   			else:
+   				num_lst = [float(item[idx]) for item in exp if item[idx] != '?']
+   				#num_lst_0 = [float(item[idx]) for item in exp if item[idx] != '?' and item[-1] == '0']
+   				mean = sum(num_lst) / len(num_lst)
+   				#mean_0 = sum(num_lst_0) / len(num_lst_0)
+   				#print mean
+   				#mode_exp = mode([item[idx] for item in exp])[0][0]
+   				for item in exp:
+   					if item[idx] == '?':
+   						item[idx] = mean
+   		return exp
+   		
     
   
 
 	def split(self, lst, idx):
 		split_candidate = []
 		for x, y in zip(lst, lst[1:]):
-			if x[idx] != y[idx] and x[-1] != y[-1]:
-			    split_candidate.append( (float(x[idx]) + float(y[idx]))/2 )
+			if (x[-1] != y[-1]) and (x[idx] != y[idx]):
+				split_candidate.append( (x[idx] + y[idx]) / 2 )
 		return split_candidate
-	'''
+	"""
 	def rep_miss(self, examples):
 		exp = copy.deepcopy(examples)
 		for attr in self.attributes:
 			idx = self.attributes.index(attr)
-			mode_exp = mode([item[idx] for item in examples])[0][0]
+			mode_1 = mode([item[idx] for item in examples if item[-1] == '1'])[0][0]
+			mode_0 = mode([item[idx] for item in examples if item[-1] == '0'])[0][0]
 			for item in exp:
 				if item[idx] == '?':
-					item[idx] = mode_exp
+					if item[-1] == '1':
+						item[idx] = mode_1
+					else:
+						item[idx] = mode_0
 		return exp
-    '''
+    """
 	def validate(self, valid_file):
 		valid_set = list(read_data(valid_file))
 		valid_data = valid_set[1:]
-		acc = 0
+		acc = 0.0
 		exp = self.rep_miss(valid_data)
 		#print exp
 		for sample in exp:
@@ -177,21 +215,28 @@ class C4_5:
 			#print 'rlt: '+rlt
 			#print 'smp: '+sample[-1]
 			if rlt == sample[-1]:
+				acc += 1.0
+		return acc/len(valid_data)
+
+	def preprocess(self, valid_file):
+		valid_set = list(read_data(valid_file))
+		valid_data = valid_set[1:]
+		exp = self.rep_miss(valid_data)
+		return exp
+
+	def valid_prune(self, exp):
+		acc = 0
+		for sample in exp:
+			if self.test(sample) == sample[-1]:
 				acc += 1
-		return float(acc)/len(valid_data)
+		return float(acc)/len(exp)
 
 	def predict(self, test_file):
 		test_set = read_data(test_file)
 		test_data = test_set[1:]
-		exp = copy.deepcopy(test_data)
-		for attr in self.attributes:
-			idx = self.attributes.index(attr)
-			mode_exp = mode([item[idx] for item in test_data])[0][0]
-			for item in exp:
-				if item[idx] == '?':
-					item[idx] = mode_exp
+		exp = self.rep_miss(test_data)
 		for i in range(0, len(exp)):
-			rlt = self.test(self, exp[i])
+			rlt = self.test(exp[i])
 			test_data[i].append(rlt)
 		header = test_set[0]
 		header.append('predict '+header[-1])
@@ -200,8 +245,8 @@ class C4_5:
 			writer = csv.writer(f)
 			writer.writerows(test_data)
 
-	"""
-	def test(self, tree, sample):
+	
+	def fast_test(self, tree, sample):
 		if tree.child:
 			if tree.child[0] == '1':
 				return '1'
@@ -213,51 +258,60 @@ class C4_5:
 						idx = self.attributes.index(item.val[0])
 						attr = sample[idx]
 						if self.format[idx] == 'nominal':
-							if eval('attr '+item.val[1]+' item.val[2]'):
-								return self.test(item, sample)
+							if attr == item.val[2]:
+								return self.fast_test(item, sample)
 						else:
-							if eval(attr+''.join(item.val[1:])):
-								return self.test(item, sample)
+							if item.val[1] == '>':
+								if float(attr) > float(item.val[2]):
+									return self.fast_test(item, sample)
+							else:
+								if float(attr) <= float(item.val[2]):
+									return self.fast_test(item, sample)
 					else:
-						return self.test(item, sample)
-    """
+						return self.fast_test(item, sample)
+    
 
 
 	def prune(self, valid_file):
+		exp = self.preprocess(valid_file)
 		for single_rule in self.rule:
-			before = self.validate(valid_file)
+			before = self.valid_prune(exp)
 			old_rule = copy.deepcopy(self.rule)
 			self.rule.remove(single_rule)
-			after = self.validate(valid_file)
+			after = self.valid_prune(exp)
 			print before
 			print after
-			if after > before:
-				self.pruning(valid_file)
-			elif after < before:
-				self.rule = copy.deepcopy(old_rule)
+			#if after > before:
+			#	self.prune(valid_file)
+			if after < before:
+				self.rule = old_rule
 
 	def test(self, sample):
+		#print '.'
 		for single_rule in self.rule:
-			r = '1'
 			for stump in single_rule:
-				#print stump
+				if stump[0] == 'class':
+					return stump[2]
 				idx = self.attributes.index(stump[0])
 				attr = sample[idx]
 				if self.format[idx] == 'nominal':
-					#print stump
-					#print ('~(attr'+stump[1]+'stump[2])')
-					#print eval('not (attr'+stump[1]+'stump[2])')
-					if eval('not (attr'+stump[1]+'stump[2])'):
-						r = '0'
+					if attr == stump[2]:
+						pass
+					else:
 						break
 				else:
-					if eval('not (float(attr)'+stump[1]+'float(stump[2]))'):
-						r = '0'
-						break
-			if r == '1':
-				return r
-		return r
-
+					if stump[1] == '>':
+						if float(attr) > float(stump[2]):
+							pass
+						else:
+							break
+					else:
+						if float(attr) <= float(stump[2]):
+							pass
+						else:
+							break
+		return '0'
+	
 
 	def __str__(self):
 		#print self.rule
@@ -265,11 +319,11 @@ class C4_5:
 		for single_rule in self.rule:
 			single_rule_str = []
 			for item in single_rule:
-				 single_rule_str.append(''.join(item))
-			rule_str.append('('+' AND '.join(single_rule_str)+')')
-		return ' OR '.join(rule_str)
+				 single_rule_str.append('('+' '.join(item)+')')
+			rule_str.append('('+' ∨ '.join(single_rule_str)+')')
+		return ' ∧ '.join(rule_str)
 
-	def rule_generator(self, tree, single_rule, flag):
+	def rule_generator(self, tree, single_rule):
 		#if flag:
 		#	self.rule = []
 		#print tree
@@ -277,12 +331,14 @@ class C4_5:
 			if isinstance(tree.val, list):
 				single_rule.append(tree.val)
 			if tree.child[0] == '1':
+				single_rule.append(['class', '=', '1'])
 				self.rule.append(single_rule)
 			elif tree.child[0] == '0':
-				pass
+				single_rule.append(['class', '=', '0'])
+				self.rule.append(single_rule)
 			else:
 				for item in tree.child:
-					self.rule_generator(item, list(single_rule), 0)
+					self.rule_generator(item, list(single_rule))
 
 	"""
 	def rule_generator(self, tree, rule_str, rule):
@@ -319,6 +375,13 @@ def unique(seq):
     for e in seq:
     	keys[e] = 1
     return keys.keys()
+
+def allequal(seq):
+	flag = seq[0]
+	for item in seq:
+		if item != flag:
+			return 0
+	return 1
 
 def mode(lst):
 	frequent = Counter(lst)
--- main.py
+++ main.py
@@ -3,33 +3,53 @@ from c4_5 import read_data
 #import operator
 
 # the main func
-def dtree(inputfile, pruning, task, outfile, format):
+def dtree(inputfile, pruning, task, validfile, testfile, format):
+    """A wrapper function using C4_5 for general train and test procedure
+    It will print best attributes during training. Print the tree generated(
+    before and after pruning). Print validate accuracy after validation and
+    during pruning.    
+
+    Args:
+        inputfile: The path of input file(training data)
+        pruning: whether implementing pruning on the tree(1 or 0)
+        task: "validate" or "predict"
+        validfile: path
+        testfile: path
+        format: the form of attributes, list of "nominal" and "numeric"
+    """
     train_set = read_data(inputfile)
     classifier = C4_5(train_set, format, [])
     classifier.train(classifier.train_set, classifier.tree, list(classifier.attributes), 0)
-    classifier.rule_generator(classifier.tree, [], 1)
+    classifier.rule_generator(classifier.tree, [])
+    print '------------------------'
     print 'without pruning'
+    print '------------------------'
     print classifier
-    print classifier.validate(outfile)
+    f = open('unprune_rule.txt', 'wb')
+    f.write(classifier.__str__())
+    f.close()
+    print classifier.validate(validfile)
     if pruning:
-        classifier.prune(outfile)
+        classifier.prune(validfile)
+        print '------------------------'
         print 'after pruning'
+        print '------------------------'
         print classifier
     if task == 'valid':
-        print classifier.validate(outfile)
+        print classifier.validate(validfile)
     elif task == 'predict':
-        classifier.predict(outfile)
+        classifier.predict(testfile)
 
 # define the format of attributes of csv data
 format = []
 for i in range(12):
     format.append("numeric")
-for i in range(3,6):
+for i in range(3,8):
     format[i] = "nominal"
 #print format
 # test for read_data
-inputfile = '/Users/wangkexue/Google Drive/14Winter/EECS 349/ps2/train.csv'
-validfile = '/Users/wangkexue/Google Drive/14Winter/EECS 349/ps2/validate.csv'
-testfile = '/Users/wangkexue/Google Drive/14Winter/EECS 349/ps2/test.csv'
-train_set = dtree(inputfile, 1, 'validate', inputfile, format)
+inputfile = '../train.csv'
+validfile = '../validate.csv'
+testfile = '../test.csv'
+train_set = dtree(inputfile, 1, 'predict', validfile, testfile, format)
 
